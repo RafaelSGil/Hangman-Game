2033ede4e4f278f70fab24df7a084c63641aa92b
Rafael <rafaelgil131@gmail.com>
2022-04-18 23:03:12 +0100

    updated

--- HangMan/src/com/rafaDEV/HangMan.java
+++ HangMan/src/com/rafaDEV/HangMan.java
@@ -1,7 +1,7 @@
 package com.rafaDEV;
 
 public class HangMan {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Model model = new Model();
         UI ui = new UI(model);
         ui.game();
